471384563e42dab5eddde767e572940358255449
Alexander Nardozzi <anardozzi@mystique.highpoint.edu>
2017-04-03 13:58:32 -0400

    completed chapter 18

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Alexander Nardozzi on 4/3/17.
+//  Copyright © 2017 CSC2310. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line{
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
